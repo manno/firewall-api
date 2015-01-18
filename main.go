8adf1f34bf1ec948a8b485c5a2e62bf32256b6b3
Mario Manno <mail@manno.name>
2015-01-18 23:39:18 +0100

    initial

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+//import "encoding/json"
+import "fmt"
+//import "os"
+
+
+// check against api keys
+// write ip to database (api, ip, old ip)
+func main() {
+  fmt.Println("asd")
+}
+
+
+// root iterate apis
+// set up iptables, remove old ip, set nil in db
