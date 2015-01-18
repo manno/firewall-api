7394964134c553d1b33ebef21b6fd2c5229c1fab
Mario Manno <mail@manno.name>
2015-01-19 00:14:40 +0100

    initial

--- main.go
+++ main.go
@@ -1,15 +1,39 @@
 package main
-//import "encoding/json"
-import "fmt"
-//import "os"
 
+//import "encoding/json"
+import (
+  "fmt"
+  "html"
+  "log"
+  "net/http"
+  "encoding/json"
+  "github.com/gorilla/mux"
+)
+//import "os"
 
 // check against api keys
 // write ip to database (api, ip, old ip)
 func main() {
-  fmt.Println("asd")
+  router := mux.NewRouter().StrictSlash(true)
+  router.HandleFunc("/", Index)
+  router.HandleFunc("/ping/{apiKey}", Ping)
+  log.Fatal(http.ListenAndServe(":8080", router))
 }
 
+
+func Index(w http.ResponseWriter, r *http.Request) {
+  users := Users{
+    User{ApiKey: "123", Ip: r.RemoteAddr},
+  }
+  json.NewEncoder(w).Encode(users)
+}
+
+func Ping(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+  apiKey := vars["apiKey"]
+  fmt.Fprintf(w, "key:", apiKey)
+  fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
 
 // root iterate apis
 // set up iptables, remove old ip, set nil in db
---
+++ models.go
@@ -0,0 +1,12 @@
+package main
+
+import "time"
+
+type User struct {
+  ApiKey        string  `json:"api_key"`
+  Ip            string  `json:"ip"`
+  OldIp         string  `json:"old_ip"`
+  UpdatedAt     time.Time  `json:"update_at"`
+}
+
+type Users []User
